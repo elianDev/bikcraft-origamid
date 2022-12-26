0c9e957abd2a82c54c8b0415f546d6484afa6664
Elian Silva <you@example.com>
2022-12-26 18:17:10 -0300

    ativado itens orçamento

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.querySelector(`#${parametro}`);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
